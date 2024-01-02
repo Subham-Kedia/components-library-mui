7519ec5ddad14749f8a586ee110f3fd0311a8198
Shubham Kedia <shubham.kedia@vegrow.in>
2024-01-02 12:01:19 +0530

    initial setup

---
+++ demo/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import { Button } from "components-library-mui";
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
